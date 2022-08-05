d98931702222ddd415f80971e726780188c7f15d
Modesto Amador <mofesdo.dev@gmail.com>
2022-08-05 10:29:48 -0700

    problem2 done

--- Aug042022/problem2.js
+++ Aug042022/problem2.js
@@ -1,20 +1,20 @@
 
 function repeat(string,times){
-    if(times>268435440){
-        times=268435440;
+    let str = '';
+    if(times > 0){
+        let count = times;
+        if(times>268435440){
+            count=268435440;
+        }
+        while(count > 1){
+            if(count & 1){
+                str += string;
+            }
+            count >>= 1;
+            string += string;
+        }
+        str += string   
     }
-    if(times==0){
-        return "";
-    }
-    let multiplier=Math.floor(Math.log2(times)); //amount of 2 to the power of 'multiplier' that fits in times
-    if(multiplier==0){
-        return string;
-    }
-    let str=string;
-    for(i=0;i<multiplier;i++){//repeat by doubling each time
-        string+=string;
-    }
-    return repeat(str,times-Math.pow(2,multiplier))+string; //recurse with remainning times
-   
+   return str;
 }
-console.log(repeat("a",268435437).length);
+console.log(repeat("a",268435441).length);
